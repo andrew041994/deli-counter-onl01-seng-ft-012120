8851a2c8a298eb8511f1783047686c2e5810eeae
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 05:55:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,13 +10,13 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(other_deli,"Grace")
+def take_a_number(other_deli,name)
   # other_deli << name
   # puts "Welcome, #{other_deli[0]}. You are number 1 in line."
     if other_deli.size==0
       other_deli<<name
       puts "Welcome, #{other_deli[0]}. You are number 1 in line."
-    else other_deli.size > 0
+    else
       other_deli.each_with_index {|name,index| puts "Welcome #{name}. You are number #{index+1} in line."}
     end
 end
