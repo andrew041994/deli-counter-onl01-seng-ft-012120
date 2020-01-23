156c5a94c901e90fb24448d7e1cf027d6a7fddf7
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 06:47:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def now_serving(deli_array)
   #binding.pry
     if deli_array.size > 0
       while deli_array.size > 0
-         "Currently serving #{deli_array.first}."
+        print "Currently serving #{deli_array.first}."
         deli_array.shift
       end
 
