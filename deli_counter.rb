f2be4f0c5e9d8feb9cbc792cfd18a01a96bcd6d0
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 04:58:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,5 @@ def line(katz_deli)
     line_array=[ "The line is currently:"]
     katz_deli.each_with_index { |name,index| line_array<< " #{index+1}. #{name}"}
     puts line_array.join
-
   end
-
 end
-# "The line is currently: 1. Ada 2. Grace 3. Kent"
