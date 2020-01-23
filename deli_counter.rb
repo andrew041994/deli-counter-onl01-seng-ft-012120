d0f11d63f02fefded103777b3b387442d9fa1736
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 04:56:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.size < 1
+    puts "The line is currently empty."
+  else
+    line_array=[ "The line is currently:"]
+    katz_deli.each_with_index { |name,index| line_array<< " #{index+1}. #{name}"}
+    puts line_array.join
+
+  end
+
+end
+# "The line is currently: 1. Ada 2. Grace 3. Kent"
