daeafdfc4dc341dfc50f32250f5663a6340d26fa
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 05:40:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,12 +11,9 @@ def line(katz_deli)
 end
 
 def take_a_number(other_deli,name)
-  message=[]
-  if other_deli.size == 0
-    other_deli << name
-  else
-    other_deli.each_with_index { |name,index| message << "Welcome, #{name}. You are number #{index+1} in line. "}
-
-  end
+  other_deli<< name
+  line_array=[ "Welcome,"]
+  other_deli.each_with_index { |name,index| line_array<< " #{name}. You are number #{index+1} in line."}
+  puts line_array.join
 
 end
